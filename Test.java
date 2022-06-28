9fa31550d9e3be909993b48edb7e2e0a44fcb2c0
dhanaraj.kumarasamy <dhanaraj.kumarasamy@reflectionsinfos.com>
2022-06-28 17:05:35 +0530

    second commit

--- Test.java
+++ Test.java
@@ -1,3 +1,12 @@
 public class Test {
 
+    public static void main(String[] args) {
+
+        // Declare the variables
+        int num;
+
+        // Input the integer
+        System.out.println("Enter the integer: ");
+    }
+
 }
